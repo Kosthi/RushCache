ab19af704d0f535ceaa620c2b2983be1e5c5eee3
Koschei <nitianzero@gmail.com>
2025-01-29 17:46:02 +0800

    feat: 实现单机并发缓存

---
+++ cache.go
@@ -0,0 +1,33 @@
+package RushCache
+
+import (
+	"RushCache/lru"
+	"sync"
+)
+
+type Cache struct {
+	mu         sync.Mutex // 互斥锁
+	lru        *lru.Cache // lru 策略缓存
+	cacheBytes int64      // 缓存的大小
+}
+
+func (c *Cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *Cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if v, ok := c.lru.Get(key); ok {
+		return v.(ByteView), ok
+	}
+	return
+}
---
+++ rushcache.go
@@ -0,0 +1,88 @@
+package RushCache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+// Getter 接口
+type Getter interface {
+	Get(key string) ([]byte, error) // 回调函数
+}
+
+// GetterFunc 函数类型
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+type Group struct {
+	name      string
+	getter    Getter
+	mainCache Cache
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("getter is nil")
+	}
+	mu.RLock()
+	defer mu.RUnlock()
+	g := &Group{
+		name:      name,
+		getter:    getter,
+		mainCache: Cache{cacheBytes: cacheBytes},
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	if g, ok := groups[name]; ok {
+		return g
+	}
+	return nil
+}
+
+func (g *Group) Name() string {
+	return g.name
+}
+
+func (g *Group) populateCache(key string, value ByteView) {
+	g.mainCache.add(key, value)
+}
+
+// getLocally 调用用户回调函数从数据源获取数据，并且将数据添加到缓存 mainCache 中（通过 populateCache 方法）
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, err := g.getter.Get(key) // 从数据源（数据库，文件等）获取数据，回调函数由用户提供
+	if err != nil {
+		return ByteView{}, err
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.populateCache(key, value)
+	return value, nil
+}
+
+func (g *Group) load(key string) (value ByteView, err error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is empty")
+	}
+	if v, ok := g.mainCache.get(key); ok {
+		log.Println("[RushCache] hit from cache", key)
+		return v, nil
+	}
+	return g.load(key)
+}
